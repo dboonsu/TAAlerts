91e86ee44ccfc5cbe678bfcbfc059533c96c5faa
unknown <dtm190000@utdallas.edu>
2021-11-13 16:29:49 -0600

    graphs 50-day and 200-day SMA

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
+
 def preprocess(name):
     temp = yf.Ticker(name)
     historical = pd.DataFrame(temp.history(period="max"))
     historical = historical.drop(columns=["Dividends", "Stock Splits"])
-    historical.to_csv("historical.csv")
+    return historical
+
+def nMovingAverage(historical, n):
+    historical[str(n) + "-day SMA"] = historical.Close.rolling(n, min_periods=n).mean()
+    return historical
 
 if __name__ == '__main__':
-    preprocess("SPY")
+    historical = preprocess("SPY")
+    historical = nMovingAverage(historical, 50)
+    historical = nMovingAverage(historical, 200)
+    historical["diff"] = historical["50-day SMA"] > historical["200-day SMA"]
+    historical["diff-next"] = historical["diff"].shift(1)
+    historical["golden-cross"] = ((historical["diff"] == True) & (historical["diff-next"] == False))
+
+    historical.to_csv("historical.csv")
+    fig, ax = plt.subplots()
+    plt.plot(historical.index, historical["Close"])
+    plt.plot(historical["50-day SMA"], "r--", label="50-day SMA")
+    plt.plot(historical["200-day SMA"], "b--", label="200-day SMA")
+    plt.show()
+
+    print(historical)
